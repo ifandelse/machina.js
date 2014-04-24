5d8b299dfbddf933288da671afd5d6c20abcd8b2
ifandelse <jim@ifandelse.com>
2014-04-24 03:05:26 -0400

    fixing whitespace issue in example

--- example/connectivity/js/connectivityFsm.js
+++ example/connectivity/js/connectivityFsm.js
@@ -1,76 +1,76 @@
-define( [
-	'jquery',
-	'machina',
-	'underscore'
-], function ( $, machina, _ ) {
+define([
+    'jquery',
+    'machina',
+    'underscore'
+], function($, machina, _) {
 
-  var useStethoscope = function ( fsm, steth ) {
-    _.each( ['heartbeat', 'no-heartbeat'], function ( eventName ) {
-      steth.on( eventName, function () {
-        fsm.handle( eventName );
-      } );
-    } );
-  };
+    var useStethoscope = function(fsm, steth) {
+        _.each(['heartbeat', 'no-heartbeat'], function(eventName) {
+            steth.on(eventName, function() {
+                fsm.handle(eventName);
+            });
+        });
+    };
 
-	return machina.Fsm.extend( {
+    return machina.Fsm.extend({
 
-			namespace : 'connectivity',
+        namespace: 'connectivity',
 
-			initialState : "offline",
+        initialState: "offline",
 
-      initialize: function() {
-        var self = this;
-        $( window ).bind( "online", function () {
-          self.handle( "window.online" );
-        } );
+        initialize: function() {
+            var self = this;
+            $(window).bind("online", function() {
+                self.handle("window.online");
+            });
 
-        $( window ).bind( "offline", function () {
-          self.handle( "window.offline" );
-        } );
+            $(window).bind("offline", function() {
+                self.handle("window.offline");
+            });
 
-        $( window.applicationCache ).bind( "error", function () {
-          self.handle( "appCache.error" );
-        } );
+            $(window.applicationCache).bind("error", function() {
+                self.handle("appCache.error");
+            });
 
-        $( window.applicationCache ).bind( "downloading", function () {
-          self.handle( "appCache.downloading" );
-        } );
-      },
+            $(window.applicationCache).bind("downloading", function() {
+                self.handle("appCache.downloading");
+            });
+        },
 
-			states : {
-				probing : {
-					_onEnter : function () {
-						if ( !this.wiredUp ) {
-							useStethoscope( this, this.stethoscope );
-              this.wiredUp = true;
-						}
-            this.stethoscope.checkHeartbeat();
-					},
-					heartbeat : "online",
-					"no-heartbeat" : "disconnected",
-					"go.offline" : "offline",
-					"*" : function () {
-						this.deferUntilTransition();
-					}
-				},
+        states: {
+            probing: {
+                _onEnter: function() {
+                    if (!this.wiredUp) {
+                        useStethoscope(this, this.stethoscope);
+                        this.wiredUp = true;
+                    }
+                    this.stethoscope.checkHeartbeat();
+                },
+                heartbeat      : "online",
+                "no-heartbeat" : "disconnected",
+                "go.offline"   : "offline",
+                "*": function() {
+                    this.deferUntilTransition();
+                }
+            },
 
-				online : {
-					"window.offline"  : "probing",
-					"appCache.error"  : "probing",
-					"request.timeout" : "probing",
-					"go.offline"      : "offline"
-				},
+            online: {
+                "window.offline"  : "probing",
+                "appCache.error"  : "probing",
+                "request.timeout" : "probing",
+                "go.offline"      : "offline"
+            },
 
-				disconnected : {
-					"window.online"        : "probing",
-					"appCache.downloading" : "probing",
-					"go.online"            : "probing",
-					"go.offline"           : "offline"
-				},
+            disconnected: {
+                "window.online"        : "probing",
+                "appCache.downloading" : "probing",
+                "go.online"            : "probing",
+                "go.offline"           : "offline"
+            },
 
-				offline : {
-					"go.online" : "probing"
-				}
-			}
-		});
+            offline: {
+                "go.online": "probing"
+            }
+        }
+    });
 });
